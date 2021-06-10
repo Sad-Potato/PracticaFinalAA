6368247ec520b1930cd996d19545e25f0837ab00
xpat <sergio100000000@gmail.com>
2021-06-10 10:08:41 +0200

    Py init

---
+++ pfinal.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sk
+
+
+
+
+
+
+
+
+
+
+
+
+# Fijamos la semilla de las 
+# componentes aleatorias
+np.random.seed(42)
+
+#---------------------------------------------------------------------------------#
+#------------------------- Lectura de datos y preprocesado -----------------------#
+#---------------------------------------------------------------------------------#
