82a9c07cfb5b184473899c7c1823dd16d3cafca4
xpat <sergio100000000@gmail.com>
2021-06-10 11:00:57 +0200

    Clases

--- pfinal.py
+++ pfinal.py
@@ -29,6 +29,9 @@ Data=np.loadtxt("./datos/letter-recognition.data",dtype=str,delimiter=",")
 
 X=np.array(Data[:,1:]).astype(np.int32)
 Y=Data[:,0]
+# Yn=np.empty(np.size(Data[:,0])) # Clases como numeros 
+# for i in np.arange(np.size(Yn)):
+#     Yn[i]=ord(Y[i])-65
 
 print(Data,X,Y)
 print(type(X))
