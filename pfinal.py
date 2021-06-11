55b0ce2e4814348c44cb885610c9c52ad131d764
Exequiel <exequielac@correo.ugr.es>
2021-06-11 19:19:28 +0200

    arreglado escalado

--- pfinal.py
+++ pfinal.py
@@ -38,11 +38,6 @@ for i in np.arange(np.size(Yn)):
     Y[i]=ord(Yn[i])-65
 
 
-# Eliminamos datos sin variabilidad si los hay (data snooping?)
-
-selector = VarianceThreshold()
-X=selector.fit_transform(X,Y)
-
 # Dividimos los datos en training y test
 '''
     Dividimos en training y test, con un tamaño de test del 20% de la muestra, 
@@ -54,46 +49,21 @@ X_train, X_test, y_train, y_test = sk.model_selection.train_test_split(X,Y,test_
 print("Tamaños de la bd:","\n\tTrain: ",np.size(X_train[:,0]),"\n\tTest: ",np.size(X_test[:,0]),"\n\tTotal: ",np.size(Y[:]))
 input("\n--- Pulsar tecla para continuar ---\n")
 
-# Pasar datos de TRAINING a pandas para exploración
 
-cabecera = ['C' + str(i) for i in range(X_train.shape[1])]    # Formateo de columnas
-cabecera.append('letra')
-dataTrain = pd.concat([pd.DataFrame(X_train), pd.DataFrame(y_train.reshape(-1, 1))], axis = 1)
-dataTrain.set_axis(cabecera, axis=1, inplace=True)
-
-
-# Eliminamos outliers
-"""
-    Eliminamos valores extremos, es decir instancias en cuales no un
-    atributo esta "alejado" de los demas si no que en su conjunto esta 
-    alejada del resto de instancias
-"""
-# OUTLIERS CON LOCAL OUTLIER FACTOR
-
-lof = LocalOutlierFactor(contamination=0.05) # Eliminamos como maximo 5% de outliers o probar con contamination='auto'? // Yo pondría como máximo 5%
-outliers=np.where(lof.fit_predict(X_train)==-1)
-X_train=np.delete(X_train,outliers,0)
-y_train=np.delete(y_train,outliers,0)
-
-
-# from sklearn.ensemble import IsolationForest
-# clf = IsolationForest(random_state=0).fit(X_train)
-# outliers=np.where(clf.predict(X_train)==-1)
-# X_train=np.delete(X_train,outliers,0)
-# y_train=np.delete(y_train,outliers,0)
-
-print("Porcentaje de outliers: ",(np.size(outliers)/np.size(X_train[:,0]))*100,"%")
-
-# Exploración y análisis de datos
+# Exploración y análisis de datos de entrenamiento
 """
     Exploración y análisis de datos
     Histograma de frecuencia de clase: sirve para visualizar y detectar clases desbalanceadas
     Boxplot de cada característica: información visual estadística de cada característica
                                     útil para ver visualmente los distintos rangos en los que se mueven las características
-    Boxplot tras estandarización: quizás debería estar en el apartado de procesado?
     Matriz de correlaciones: información visual de la correlación entre variables
-    Estadísticos de cada característico: información estadística
 """ 
+
+# # Pasar datos a dataframe de pandas
+# cabecera = ['C' + str(i) for i in range(X_train.shape[1])]    # Formateo de columnas
+# cabecera.append('letra')
+# dataTrain = pd.concat([pd.DataFrame(X_train), pd.DataFrame(y_train.reshape(-1, 1))], axis = 1)
+# dataTrain.set_axis(cabecera, axis=1, inplace=True)
 
 # # Histograma de frecuencia de cada clase
 # plt.figure(figsize = (9, 6))
@@ -107,34 +77,13 @@ print("Porcentaje de outliers: ",(np.size(outliers)/np.size(X_train[:,0]))*100,"
 # input("\n--- Pulsar tecla para continuar ---\n")
 
 # # Boxplot de las distintas características
-# plt.figure(figsize = (20, 10))
+# plt.figure(figsize = (14, 7))
 # plt.xlabel("Características del problema")
 # boxplot = dataTrain[dataTrain.columns[0:-1]].boxplot()
 # plt.title("Boxplot de cada característica")
 # plt.show()
 
 # print("Boxplot para cada característica del problema")
-# input("\n--- Pulsar tecla para continuar ---\n")
-
-# Muy poca información visual debido a valores anormales
-# Hay que estandarizar los datos
-scaler = sk.preprocessing.StandardScaler().fit(X_train)
-X_train_scaled = scaler.transform(X_train)
-dataTrainScaled = pd.DataFrame(np.concatenate((X_train_scaled, y_train.reshape(-1,1)), axis = 1),
-                               columns = dataTrain.columns)
-
-# normalizamos tambien el test?¿ Sí, pero al final para que Nicolás no se raye
-X_test=scaler.transform(X_test)
-                            
-
-# # Boxplot de las distintas variables continuas tras estandarización
-# plt.figure(figsize = (20, 10))
-# plt.xlabel("Variables del problema")
-# boxplot = dataTrainScaled[dataTrainScaled.columns[0:-1]].boxplot()
-# plt.title("Boxplot de cada variable tras estandarización")
-# plt.show()
-
-# print("Boxplot para cada característica del problema tras estandarización")
 # input("\n--- Pulsar tecla para continuar ---\n")
 
 # # Mostrar gráfica de la matriz de correlaciones
@@ -150,6 +99,67 @@ X_test=scaler.transform(X_test)
 # print("Gráfica de la matriz de correlaciones")
 # input("\n--- Pulsar tecla para continuar ---\n")
 
+"""
+    Preprocesado de los datos
+"""
+
+# Escalado de características
+"""
+    Para escalar las características se usa estandarización, ya que los datos
+    presentan pequeñas diferencias de escalas
+"""
+# Estandarización con StandardScaler
+scaler = sk.preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+# dataTrainScaled = pd.DataFrame(np.concatenate((X_train, y_train.reshape(-1,1)), axis = 1),
+#                                columns = dataTrain.columns)
+                            
+# # Boxplot de las distintas variables continuas tras estandarización
+# plt.figure(figsize = (14, 7))
+# plt.xlabel("Características del problema")
+# boxplot = dataTrainScaled[dataTrainScaled.columns[0:-1]].boxplot()
+# plt.title("Boxplot de cada variable tras estandarización")
+# plt.show()
+
+# print("Estandarización de los datos")
+# print("Boxplot para cada característica del problema tras estandarización")
+# input("\n--- Pulsar tecla para continuar ---\n")
+
+
+# Eliminamos outliers
+"""
+    Eliminamos valores extremos, es decir instancias en cuales no un
+    atributo esta "alejado" de los demas si no que en su conjunto esta 
+    alejada del resto de instancias
+"""
+# OUTLIERS CON LOCAL OUTLIER FACTOR
+
+lof = LocalOutlierFactor(contamination=0.05)
+outliers=np.where(lof.fit_predict(X_train)==-1)
+X_train=np.delete(X_train,outliers,0)
+y_train=np.delete(y_train,outliers,0)
+
+# # Obtener nuevo dataframe tras eliminación de outliers
+# dataTrain = pd.concat([pd.DataFrame(X_train), pd.DataFrame(y_train.reshape(-1, 1))], axis = 1)
+# dataTrain.set_axis(cabecera, axis=1, inplace=True)
+
+# # Histograma de frecuencia de cada clase después de eliminar outliers
+# plt.figure(figsize = (9, 6))
+# ax = dataTrain['letra'].plot.hist(bins = np.arange(27) - 0.5, ec = 'black', xticks=np.arange(26), rot = 45)
+# ax.set_xlabel("Clases")
+# ax.set_ylabel("Frecuencia")
+# plt.title("Histograma de frecuencia")
+# plt.show()
+
+# from sklearn.ensemble import IsolationForest
+# clf = IsolationForest(random_state=0).fit(X_train)
+# outliers=np.where(clf.predict(X_train)==-1)
+# X_train=np.delete(X_train,outliers,0)
+# y_train=np.delete(y_train,outliers,0)
+
+print("Porcentaje de outliers: ",(np.size(outliers)/np.size(X_train[:,0]))*100,"%")
+print("Histograma de frecuencias agrupado por clases después de eliminar outliers")
+input("\n--- Pulsar tecla para continuar ---\n")
 
 # Tecnica de reduccion de dimensionalidad PCA / ?¿ Solo si me mejoran los resultados (guion)
 """
@@ -164,15 +174,17 @@ pca = PCA(0.95,svd_solver='full')
 dim_antes=np.shape(X_train)[1]
 X_train = pca.fit_transform(X_train)
 dim_despues=np.shape(X_train)[1]
-X_test=pca.transform(X_test)
 print("Aportación de los componentes: [",pca.explained_variance_ratio_[0],",",pca.explained_variance_ratio_[1],"...]")
 print("Reducción de dimensionalidad: ",dim_antes," -> ",dim_despues)
 
 plt.scatter(X_train[:,0],X_train[:,1],c=y_train) # Los puntos no dan mucha información no? Una alternativa sería t-SNE pero Nicolás igual se raya
 plt.show()
 
-input("\n--- Pulsar tecla para continuar ---\n")
+ax = plt.axes(projection='3d')
+ax.scatter3D(X_train[:,0], X_train[:,1], X_train[:,2], c=y_train);
+plt.show()
 
+input("\n--- Pulsar tecla para continuar ---\n")
 
 
 #####################
@@ -217,25 +229,28 @@ from sklearn.model_selection import GridSearchCV
 ##################### Perceptron multicapa #########################
 
 
-parameters = {'hidden_layer_sizes':[(50,50)],'activation':['tanh'],'solver':['adam'],
-            'learning_rate':['adaptive'],'shuffle':[True],'max_iter':[100],'n_iter_no_change':[3],'alpha':[0.001]} # Faltan
-mlp = MLPClassifier()
-clf = GridSearchCV(mlp,parameters,cv=5)
-clf.fit(X_train, y_train)
+# parameters = {'hidden_layer_sizes':[(50,50)],'activation':['tanh'],'solver':['adam'],
+#             'learning_rate':['adaptive'],'shuffle':[True],'max_iter':[100],'n_iter_no_change':[3],'alpha':[0.001]} # Faltan
+# mlp = MLPClassifier()
+# clf = GridSearchCV(mlp,parameters,cv=5)
+# clf.fit(X_train, y_train)
 
-# mlp=MLPClassifier((50,50))
-# mlp.fit(X_train,y_train)
-# print(mlp.score(X_train,y_train))
+mlp=MLPClassifier((100, 100), solver = 'adam', batch_size = 64, alpha = 0.01)
+mlp.fit(X_train,y_train)
+print(mlp.score(X_train,y_train))
+X_test = scaler.transform(X_test)
+X_test = pca.transform(X_test)
+print(mlp.score(X_test, y_test))
 
-# Código para visualizar los resultados de grid search
-print("Grid scores on development set:")
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
+# # Código para visualizar los resultados de grid search
+# print("Grid scores on development set:")
+# means = clf.cv_results_['mean_test_score']
+# stds = clf.cv_results_['std_test_score']
 
-#THIS IS WHAT YOU WANT
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r"
-          % (mean, std * 2, params))
+# #THIS IS WHAT YOU WANT
+# for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+#     print("%0.3f (+/-%0.03f) for %r"
+#           % (mean, std * 2, params))
 
 
 
